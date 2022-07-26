31069cacfce42a33203d10ae49628a8b80fad58f
Margarita Sophia <margarita.sophia@gmail.com>
2022-07-26 15:57:30 +0200

    JS update

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ let turnRed = 0;
 let eachH = 0;
 
 function startChange() {
-    turnRed = setInterval(colorChange, 1000) 
+    turnRed = setInterval(colorChange, 1000)
+
+    function colorChange() {
+        eachH %= headingsColor.length;
+        headingsColor[eachH].style.color = 'red';
+        eachH++;
+    }
 }
 
-function colorChange () {
-    eachH %= headingsColor.length;
-    headingsColor[eachH].style.color = 'red';
-    eachH ++;
-        }
-        
-      
 function stopChange() {
-    clearInterval(turnRed);
-    Array.from(headingsColor).map((heading, i) => 
-    heading.style.color = 'black')
+    clearInterval(turnRed)
+    Array.from(headingsColor).map((heading, i) =>
+        heading.style.color = 'black')
     eachH = 0;
- }
+}
